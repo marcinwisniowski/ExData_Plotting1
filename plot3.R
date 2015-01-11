e7cf98b2e439de1556dc0f72d07cccccdc5a0819
Marcin Wisniowski <marcin.wisniowski@gmail.com>
2015-01-12 00:48:33 +0100

    Solution for Plot 3

---
+++ plot3.R
@@ -0,0 +1,24 @@
+## Coursera: Data Science Specialization
+## Module: Exploratory Data Analysis
+## Project: Peer Assessment 1
+## Author: Marcin Wiśniowski
+## Date: 2015-01-11
+
+## Plot 3
+
+source('read_data.R')
+
+my_data <- read_project_data("household_power_consumption.txt", "^[12]/2/2007")
+days <- strptime(paste(my_data$Date, my_data$Time, sep=" "), format="%Y-%m-%d %T")
+
+# Create plot in PNG device
+png("plot3.png", width=480, height=480, units="px")
+
+plot(days, my_data$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(days, my_data$Sub_metering_2, col="red")
+lines(days, my_data$Sub_metering_3, col="blue")
+
+legend("topright", lty=1,col = c("black", "red", "blue"),
+       legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+
+dev.off()
